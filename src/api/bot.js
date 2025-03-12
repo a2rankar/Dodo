0fad67a350b2d3ffe53e26e4e0b13c5aaffb0176
Umar <umarsabyrbekov123@gmail.com>
2025-03-12 09:51:19 +0600

    add bot.js and removed server.js2

---
+++ src/api/bot.js
@@ -0,0 +1,18 @@
+import TelegramBot from "node-telegram-bot-api";
+import "dotenv/config";
+
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token);
+
+export default function handler(req, res) {
+    if (req.method === "POST") {
+        console.log("Received Telegram request:", req.body);
+        bot.processUpdate(req.body);
+        res.status(200).send("OK");
+        console.log('succesfully connected!')
+    } else {
+        res.status(405).send("Method Not Allowed");
+    }
+}
+
+bot.setWebHook(`https://dodo-2jxs.vercel.app/api/bot`);
